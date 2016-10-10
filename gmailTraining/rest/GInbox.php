2bb2e7628c143a3b83e3b31cad6668ff29136be3
ashwani-contentsphere <ashwani@contentsphere.com>
2016-10-10 19:31:12 +0530

    Gmail Training

--- gmailTraining/rest/GInbox.php
+++ gmailTraining/rest/GInbox.php
@@ -7,72 +7,73 @@
 Class Ginbox
 {
     private $inmessages;
-   /*
+   /**
    /*
     * reads the json file and then returns the all contents of json file
     */
     public function getInboxMessages()
     {
-        $inmessages = file_get_contents('data1.json');
+        $inmessages = file_get_contents('../rest/data/data1.json');
         file_put_contents("results.txt", print_r($inmessages, 1), 8);
         return $inmessages;
     }
-    /*
+    /**
      * read json file then converts in to php array
      * then returns the results
      */
     public function getInboxMessage($id)
     {
-        $str = file_get_contents('data1.json');
+        $str = file_get_contents('../rest/data/data1.json');
         $array = json_decode($str, true);
         $inmessages = (object)$array[$id-1];
         $data = json_encode((array)$inmessages);
         return $data;
 
     }
-    /*
+    /**
      * this function save the sent mail of the user in to a file
      * simply appends the data
      */
     public function saveSentMail($data){
-        $myfile = fopen("newfile.txt", "a");
+        $myfile = fopen("../rest/data/newfile.txt", "a");
         fwrite($myfile,$data);
     }
-    /*
+    /**
      * returns the all sent mails data in to json format
      */
     public function getSentMails()
     {
 
-        $str = file_get_contents("newfile.txt");
+        $str = file_get_contents("../rest/data/newfile.txt");
         $str=$str."]";
         return $str;
     }
-    /*
+    /**
      * returns the particular sent mail data in json format
      */
     public function getSentMail($id)
     {
-        $str = file_get_contents("newfile.txt");
+        $str = file_get_contents("../rest/data/newfile.txt");
         $str=$str."]";
         $array = json_decode($str, true);
         $inmessages = (object)$array[$id];
         $data = json_encode((array)$inmessages);
          return $data;
     }
-    /*
+    /**
      * this is the search function for searching mails
      *  it finds the matching results and  returns matching results
      */
     public function searchMails($string)
     {
 
-        $str = file_get_contents('data1.json');
+        $str = file_get_contents('../rest/data/data1.json');
         $array = json_decode($str, true);
         $data="[";
         for($i=0;$i<count($array);$i++)
         {
-            if (strpos($array[$i]["firstName"], $string) !== false||strpos($array[$i]["message"], $string) !== false)
+            if (strpos(strtolower($array[$i]["firstName"]), strtolower($string)) !== false||
+                strpos(strtolower($array[$i]["message"]), strtolower($string)) !== false)
             {
                 $data=$data.'{"id":'.'"'.$array[$i]["id"].'"'.','.'"firstName":'.'"'.$array[$i]["firstName"].
                     '"'.','.'"message":'.'"'.$array[$i]["message"].'"'.'},';
--- gmailTraining/webapp/script/script.js
+++ gmailTraining/webapp/script/script.js
@@ -3,8 +3,9 @@ $(function () {
     view.renderComponents();
 });
 
+
 /**
- * this function interacts with the server
+ * this class interacts with the server
  */
 function interactor() {
 
@@ -18,6 +19,17 @@ function interactor() {
             }
         });
     };
+
+    this.post=function(url,data,view){
+        $.post(
+            url,
+            {data:data,view:view},
+            function (data) {
+                alert(data);
+            }
+        );
+
+    };
 }
 /**
  * this class controls the functionality
@@ -26,55 +38,81 @@ function presenter() {
     var interact = new interactor();
     var v = new View();
 
-    /*
+    /**
     this function gets the all inbox messages of the user from the server
      */
+    
     this.getAllmails = function () {
-        interact.ajax("Controller.php/getData?view=all", "get", this.dataSuccessAllMailsCallback);
+        interact.ajax("../../rest/Controller.php/getData?view=all", "get", this.dataSuccessAllMailsCallback);
     };
-    /*
+    
+    /**
      this function gets the particular messages of the user from the server
      */
     this.getSingleMail = function (id) {
-        interact.ajax("Controller.php/getData?view=single&id="+id, "get", this.dataSuccessSingleMailCallback);
+        interact.ajax("../../rest/Controller.php/getData?view=single&id="+id, "get", this.dataSuccessSingleMailCallback);
     };
-    /*
+    
+    /**
      this function gets the all mails data from the server which is sent by the user
      */
     this.getAllSentMails = function () {
-        interact.ajax("Controller.php/getData?view=sentmails", "get", this.dataSuccessAllSentMailsCallback);
+        interact.ajax("../../rest/Controller.php/getData?view=sentmails", "get", this.dataSuccessAllSentMailsCallback);
     };
-    /*
+    
+    /**
      this function gets the particular mail data from the server which is sent by the user
      */
     this.getSentMail = function (id) {
 
-        interact.ajax("Controller.php/getData?view=sentmail&id=" + id, "get", this.dataSuccessSentMail);
+        interact.ajax("../../rest/Controller.php/getData?view=sentmail&id=" + id, "get", this.dataSuccessSentMail);
     };
-    /*
+
+    /**
      this function sends the query text for finding the mails and gets the search results back from the server
      */
     this.searchMail = function (string) {
-        interact.ajax("Controller.php/getData?view=search&q=" + string, "get", this.dataSuccessAllMailsCallback);
+        interact.ajax("../../rest/Controller.php/getData?view=search&q=" + string, "get", this.dataSuccessAllMailsCallback);
     };
-    /*
+
+    this.sendMail=function(data){
+        debugger;
+        interact.post("../../rest/Controller.php",data,"savesent");
+    };
+
+    /**
     the callback function for getting all mails from server and
-    when request is completed then it calls the rendering function to renders the data
+    when request is completed then it calls the rendering function to render the data
      */
     this.dataSuccessAllMailsCallback = function (json) {
         for (var i = 0; i < json.length; i++)
             v.renderAllMails(json[i], i);
-        v.bindEventsOnInbox();
+
 
     };
+    /**
+     * When request is for single mail this function is called
+     * @param json gets the data of Mail
+     */
+
     this.dataSuccessSingleMailCallback = function (json) {
         v.renderSingleMail(json);
     };
+
+    /**
+     * When request is for all Sent mails this function is called
+     * @param json gets the data of Mails
+     */
     this.dataSuccessAllSentMailsCallback = function (json) {
         for (var i = json.length - 1; i >= 0; i--)
             v.renderAllSentMails(json[i], i);
-        v.bindEventsOnSentMails();
+
     };
+
+    /**
+     * When request is for single sent mail this function is called
+     * @param json gets the data of Mail
+     */
     this.dataSuccessSentMail = function (json) {
         v.renderSentMail(json);
     }
@@ -86,23 +124,26 @@ function presenter() {
  * this function renders the whole page
  * it contains all event listners
  * @example appendleft() renders the left part of the page
- * @exapmle bindsEventsOnButton()  attachs the particular event on components
+ * @exapmle bindsEventsOnButton()  attach the particular event on components
  **/
 
 function View() {
     /**
     this function renders the left part of the page
     */
-    this.appendLeft = function () {
+    
+    this.appendLeftMenu = function () {
         $('#wrapper').append("<div id='first'><button type='button' id='compose'>compose</button>" +
             "<button type='button' id='inbox'>inbox (2)</button>" +
             "<button type='button' id='sentmails'>Sent Mails</button>"
             + "<div id='D'>Starred</div>" + "<div id='D'>Drafts</div>" +
             "<div id='D'>Spam</div>");
-        /**
-         *  this part of function the more elements div
-         */
 
+
+        this.inboxButtonEvent();
+        this.sentMailsButtonEvent();
+
+        //this part of function the more elements div
         $('#first').append(
             "<div id='more'>More ></div>" +
             "<div id='less'>Less ></div><div id='moreitems'>" +
@@ -110,26 +151,34 @@ function View() {
             "<div id='moreitems1'>All mails" +
             "</div><div id='moreitems1'>Spam</div><div id='moreitems1'>Trash</div>" +
             "<div id='moreitems1'>Categories</div></div>");
+
+        this.moreButtonEvents();
     };
+    
     /**
      * this function renders the header of page
      */
-    this.appendHeader = function () {
+    this.appendSearchHeaderContainer = function () {
         $('#wrapper').append(" <div id='second'>" +
-            "<img src='gmail.jpg'  width='42' height='42' id='logo'>" +
+            "<img src='../resources/gmail.jpg'  width='42' height='42' id='logo'>" +
             " <input type='text' name='search' id='find'>" +
-            "<button type='button' id='search'>search</button><br></div>")
+            "<button type='button' id='search'>search</button><br></div>");
+
+       this.searchMailsEvent();
+
     };
+    
     /**
      * this function renders the second header of the page
      */
-    this.appendHeader2 = function () {
+    this.appendSettingsHeaderContainer = function () {
         $('#wrapper').append("<div id='middle'><div id='middle1'>Mail </div>" +
             "<div id='middle20'><div id='middle2'>" +
             "<input type='checkbox'></div><div id='middle2'></div><div id='middle2'>More</div>" +
             "</div><div id='middle3'></div>" +
             "<div id='middle4'></div><div id='middle5'></div></div>")
     };
+    
     /**
      * this funciton renders the body of the page
      * and renders the dialog for sending a mail
@@ -140,8 +189,13 @@ function View() {
             "<input type='text' id='subject' value='Subject'>" +
             "<input type='text' id='msg' >" +
             "<button type='button' id='send'>send</button>" +
-            "</div>")
-    }
+            "</div>");
+
+           this.sendMailEvent();
+           this.composeButtonEvent();
+
+       };
+    
     /**
      * this function renders all the mails of the users and it receives two parameters
      * @param json  it is main user (containing mail information) data to be render on body
@@ -151,7 +205,9 @@ function View() {
             "<div id='checkboxmsg'><input type='checkbox'></div>" +
             "<h3>" + json.firstName + "</h3></div><div " +
             " id='" + json.id + "' ><h3>  " + json.message + "</h3></div></div>");
+             this.bindEventsOnInbox(id);
     };
+    
     /**
      * this function renders the complete mail on the body
      * @param jsonit is main user (containing mail information) data to be render on body
@@ -160,49 +216,54 @@ function View() {
             $("#third").append("<h1>Name :" + json.firstName + "</h1><br><h2>" + "Message : " + json.message + "</h2>" +
             "<textarea rows='6' cols='80'>Click here to reply or Forward</textarea>" +
             "<div id='reply'></div><div id='replyButton'>send</div>");
-    };
+    }
+    
     /**
      * this function renders all the mails sent by the user and its receives two parameters
      * @param json  it is main user (containing mail information) data to be render on body
      */
     this.renderAllSentMails = function (json, id) {
 
-        $("#third").append("<div class='messages' id='" + id + "' >" +
-            "<h3>" + "to :" + json.to + "  " + "Subject  :" + json.subject + " Message  "
-            + json.Message + "</h3></div>");
+        $("#third").append("<div class='messages' id='" + id + "' ><div id='checkboxmsg'><input type='checkbox'></div>" +
+            "<div class='messages0'><h3>" + "to :" + json.to + "</h3></div>  <div><h3> " + " Subject  :" + json.subject +
+            "</h3></div><div><h3> "+"-"
+            + json.Message + "</h3></div></div>");
+        this.bindEventsOnSentMails(id);
     };
+    
     /**
      * this function binds the events on inbox messages
      * @example when clicks on the inbox message users gets  its contents of its  inbox messege
      */
-    this.bindEventsOnInbox = function () {
-        $(".messages").hover(
+    this.bindEventsOnInbox = function (id) {
+        $("#"+id+"").hover(
             function () {
                 $(this).css("background-color", "#F4FBFC");
             }, function () {
                 $(this).css("background-color", "rgb(236, 238, 239)");
             }
         );
-        $(".messages").click(function () {
+        $("#"+id+"").click(function () {
             var x = this.id;
             $("#third").empty();
             var p = new presenter();
             p.getSingleMail(x);
         });
     };
+    
     /**
      * this function binds the events on sent messages
      * @example when clicks on the sent message users gets  its contents of its  sent messege
      */
-    this.bindEventsOnSentMails = function () {
-        $(".messages").hover(
+    this.bindEventsOnSentMails = function (id) {
+        $("#"+id+"").hover(
             function () {
                 $(this).css("background-color", "#F4FBFC");
             }, function () {
                 $(this).css("background-color", "rgb(236, 238, 239)");
             }
         );
-        $(".messages").click(function () {
+        $("#"+id+"").click(function () {
             var x = this.id;
             $("#third").empty();
             var p = new presenter();
@@ -210,6 +271,7 @@ function View() {
 
         });
     };
+    
     /**
      * this function renders whole body of the  mail which is sent by the user
      * @param json
@@ -221,6 +283,7 @@ function View() {
             "<textarea rows='6' cols='80'>Click here to reply or Forward</textarea>" +
             "<div id='reply'></div><div id='replyButton'>send</div>");
     };
+    
     /**
      * this function adds the functionality to more  and less button
      * @example when user clicks on more button then menu will be expanded
@@ -240,6 +303,7 @@ function View() {
             $("#less").hide();
         });
     };
+
     /**
      * when user press the on the compose button then this dialog will be displayed for compose a mail
      */
@@ -251,73 +315,85 @@ function View() {
             $("#dialog").dialog("open");
         });
     };
+    
     /**
      * this function attachs the event to inbox button
      * on click the page will refreshed
      */
+    
     this.inboxButtonEvent = function () {
         $("#inbox").click(function () {
             location.reload();
         });
+        $('#to,#subject').one('click', function(){
+            this.value = '';
+        });
+
     };
+    
     /**
      * this function registers a event on send button
      * @example on click the contents of composed mail is post to the server
      */
     this.sendMailEvent = function () {
         $("#send").click(function () {
-            var to = document.getElementById("to").value;
-            var subject = document.getElementById("subject").value;
-            var msg = document.getElementById("msg").value;
-
-            $.post(
-                "Controller.php",
-                {to: to, subject: subject, msg: msg, view: "savesent"},
-                function (data) {
-                    alert(data);
-                }
-            );
+            $('#to,#subject').one('click', function(){
+                this.value = '';
+            });
+            var to = '"'+document.getElementById("to").value+'"';
+            var subject = '"'+document.getElementById("subject").value+'"';
+            var msg = '"'+document.getElementById("msg").value+'"';
+             var data="{"+'"to":'+to+',"subject":'+subject+',"Message":'+msg+"}";
+            var p = new presenter();
+            p.sendMail(data);
         });
     };
+    
     /**
      * this function attachs a event on sentmails button
      * on click user gets mails which is sent by him/her
      */
     this.sentMailsButtonEvent = function () {
         $("#sentmails").click(function () {
+            $("#inbox").css("color","black");
+            $("#sentmails").css({"color":"#e05f5f","font-weight":"bold"});
             var p = new presenter();
             $("#third").empty();
             p.getAllSentMails();
         });
     };
+    
     /**
      * this function adds the functionality for searching mail
      * @param string it recieves the text to be search  from searchBar
      */
-    this.searchMailsEvent = function (string) {
+    this.searchMailsEvent = function () {
         $("#search").click(function () {
             var string = document.getElementById("find").value;
             var p = new presenter();
             $("#third").empty();
             p.searchMail(string);
         });
+        $("#find").keypress(function(e) {
+            if(e.which == 13) {
+                var string = document.getElementById("find").value;
+                var p = new presenter();
+                $("#third").empty();
+                p.searchMail(string);
+            }
+        });
     };
+
     /**
      * this is the main function for rendering the all components of the page
      */
     this.renderComponents = function () {
-        this.appendHeader();
-        this.appendHeader2();
-        this.appendLeft();
+        this.appendSearchHeaderContainer();
+        this.appendSettingsHeaderContainer();
+        this.appendLeftMenu();
         this.appendBody();
-        this.moreButtonEvents();
-        this.composeButtonEvent();
-        this.inboxButtonEvent();
-        this.sendMailEvent();
-        this.sentMailsButtonEvent();
-        this.searchMailsEvent();
-        var present = new presenter();
-        present.getAllmails();
+        var oPresenter = new presenter();
+        oPresenter.getAllmails();
 
     };
 }
